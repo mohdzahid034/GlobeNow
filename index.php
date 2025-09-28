9e1fa418e4f01b105a683560cf9961c0be08e971
mohdzahid034 <mohdansari0935@gmail.com>
2025-09-28 22:39:34 +0530

    hhfi

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+    <h1>hello zahid</h1>
+    <p>are you ready</p>
+    <h2>hi</h2>
+    <a href "https://hpanel.hostinger.com/websites">hostinger</a>
+
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            the_title('<h2>', '</h2>');
+            the_content();
+        endwhile;
+    else :
+        echo '<p>No content found.</p>';
+    endif;
+    ?>
+</body>
+</html>
