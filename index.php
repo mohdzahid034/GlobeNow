73fa941a18d9a5b7142f28969fac54f5b373a6e5
mohdzahid034 <mohdansari0935@gmail.com>
2025-09-29 01:56:56 +0530

    gfgfuigbuiv

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="C:\Users\use\Desktop\GlobeNow\style.css">
 </head>
 <body>
-    <h1>hello zahid</h1>
+    <header>
+        <div class= "navbar">
+            <div class= "logo"><img src ="C:\Users\use\Desktop\GlobeNow\images\logo.png"></div>
+            <div class= "menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About</a></li>
+        </div>
+    </header>
+    
+    <h1>hello</h1>
     <p>are you ready</p>
-    <h2>by</h2>
+    <h2>hi</h2>
     <a href "https://hpanel.hostinger.com/websites">hostinger</a>
 
     <?php
